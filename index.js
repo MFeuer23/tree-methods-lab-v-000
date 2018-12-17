b77fede1d0bafaf9da4e90a10c3e9431f970f7aa
Mallory Feuer <mallory.feuer@gmail.com>
2018-12-17 16:07:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,5 +29,11 @@ function findOrAdd(rootNode, newNode){
 }
 
 function max(node) {
-  
+  let rightNode = node
+  if (node.right) {
+    rightNode = node.right
+    max(rightNode)
+  } else {
+    return rightNode
+  }
 }
