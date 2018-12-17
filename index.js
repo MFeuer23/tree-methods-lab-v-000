d54eea82da952a039a84abce61f15537998efb33
Mallory Feuer <mallory.feuer@gmail.com>
2018-12-17 15:54:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function findOrAdd(rootNode, newNode){
       currentNode = rootNode.left
       if(currentNode){
         findOrAdd(currentNode, newNode)
-      } else if (currentNode.data == newNode.data) {
+      } else if (currentNode == newNode) {
         return true
       } else {
         rootNode.left = newNode
@@ -23,7 +23,7 @@ function findOrAdd(rootNode, newNode){
     currentNode = rootNode.right
     if(currentNode){
       findOrAdd(currentNode, newNode)
-    } else if (currentNode.data == newNode.data) {
+    } else if (currentNode == newNode) {
       return true
     } else {
       rootNode.right = newNode
